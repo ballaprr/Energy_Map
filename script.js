a25223f8043f07f635b3319ceda37553f2942c45
ballaprr <72662542+ballaprr@users.noreply.github.com>
2022-06-30 18:34:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,39 +3,38 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
     center: [-117.141212, 32.722335],
-    zoom: 11.5
+    zoom: 12.5
 })
 
-// navigator.geolocation.getCurrentPosition(successLocation, 
-// errorLocation, {
-//     enableHighAccuracy: true
-// })
+navigator.geolocation.getCurrentPosition(successLocation, 
+errorLocation, {
+     enableHighAccuracy: true
+})
 
-// function successLocation(position) {
-//     setupMap([position.coords.longitude, position.coords.latitude])
-// }
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
 
-// function errorLocation() {}
+function errorLocation() {}
 
 
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center, 
+        zoom: 15
+})
 
-// function setupMap(center) {
-//    const map = new mapboxgl.Map({
-//        container: 'map',
-//        style: 'mapbox://styles/mapbox/streets-v11',
-//        center: center, 
-//        zoom: 15
-//    })
-
-//    const nav = new mapboxgl.NavigationControl();
-//    map.addControl(nav).addTo(map)
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav).addTo(map)
 
 //    var directions = new MapboxDireactions({
 //        accessToken: mapboxgl.accessToken
 //    }).addTo(map)
 
 //    map.addControl(directions, "top-left")
-//}
+}
 
 var marker = new mapboxgl.Marker()
     .setLngLat([-117.141212, 32.722335])
@@ -44,7 +43,5 @@ var marker = new mapboxgl.Marker()
 
 const nav = new mapboxgl.NavigationControl();
 map.addControl(nav)
-
-
 
 
